0258d0da38386baa88674ee1cece8cde7d3d8371
ARUN NOOB <83957038+arunsingh-creator@users.noreply.github.com>
2023-06-21 20:51:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,9 @@ bc_hdr = {"BCOV-POLICY": BCOV_POLICY}
 
 @bot.on_message(filters.command(["cw"]))
 async def account_login(bot: Client, m: Message):
+    global cancel
+    cancel = False
+    
     url = "https://elearn.crwilladmin.com/api/v3/login-other"
     data = {
         "deviceType": "android",
