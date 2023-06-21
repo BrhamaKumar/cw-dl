86b2ef045a6974e2198e33908f7dd3270384751f
ARUN NOOB <83957038+arunsingh-creator@users.noreply.github.com>
2023-06-21 21:05:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ async def account_login(bot: Client, m: Message):
            await m.reply_text("go back to response")
     else:
       token = raw_text
-    html1 = s.get("https://elearn.crwilladmin.com/api/v3/comp/my-batch?&token=" + token).json()
+    html1 = s.get("https://elearn.crwilladmin.com/api/v3/comp/my-batch?&token="+token).json()
     topicid = html1["data"]["batchData"]
     cool=""
     for data in topicid:
